737b3c1f6b30d8f09445db270dc201c0788fa3b2
Xaver Wenhart <125409009+xavwe@users.noreply.github.com>
2025-05-11 20:33:52 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,9 @@
 vim.cmd("set clipboard=wl-clipboard");
 vim.cmd("set number");
+vim.cmd("set expandtab");
+vim.cmd("set tabstop=2");
+vim.cmd("set softtabstop=2");
+vim.cmd("set shiftwidth=2");
 
 -- source: https://vi.stackexchange.com/questions/31811/neovim-lua-config-how-to-append-to-listchars
 vim.opt.listchars = { space = '•', tab = '>~', eol = '↵' }
