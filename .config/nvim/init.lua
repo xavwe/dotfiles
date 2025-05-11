20561722fc9a0b0b2a8572e6db062e2b9dcdff17
Xaver Wenhart <125409009+xavwe@users.noreply.github.com>
2025-05-11 20:34:11 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,4 @@
-vim.cmd("set clipboard=wl-clipboard");
+--- vim.cmd("set clipboard=wl-clipboard"); --- not needed https://www.reddit.com/r/Fedora/comments/ax9p9t/vim_and_system_clipboard_under_wayland/?rdt=44798
 vim.cmd("set number");
 vim.cmd("set expandtab");
 vim.cmd("set tabstop=2");
