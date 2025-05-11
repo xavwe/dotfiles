91a90f0785813fa65383d18f8a6e65b7a196c590
Xaver Wenhart <125409009+xavwe@users.noreply.github.com>
2025-05-11 18:36:05 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,6 @@
 vim.cmd("set clipboard=wl-clipboard");
 vim.cmd("set number");
+
+-- source: https://vi.stackexchange.com/questions/31811/neovim-lua-config-how-to-append-to-listchars
+vim.opt.listchars = { space = '•', tab = '>~', eol = '↵' }
+vim.opt.list = true
